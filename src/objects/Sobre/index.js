3f41b1fe86663a79b551f3692fb9fe5831059287
mmn92 <matheus.mota.nogueira@gmail.com>
2019-10-09 16:56:34 -0300

    cria object sobre

---
+++ src/objects/Sobre/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Sobre = () => {
+  return (
+    <a className="sobre" href="#to-do">
+      Sobre
+    </a>
+  );
+};
+
+export default Sobre;
