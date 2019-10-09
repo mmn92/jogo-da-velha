cff78e9625c5eacb5223431126b630644f23adab
mmn92 <matheus.mota.nogueira@gmail.com>
2019-10-09 14:58:38 -0300

    renomeia arquivo index.css para base.css na pasta elements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles/generic/reset.css";
-import "./styles/elements/index.css";
+import "./styles/elements/base.css";
 import "./index.css";
 import App from "./App";
 
