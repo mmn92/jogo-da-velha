8909bd5a6e66c1041aa00b8729554274e3a98a7f
mmn92 <matheus.mota.nogueira@gmail.com>
2019-10-09 17:32:11 -0300

    cria o object Logo

---
+++ src/objects/Logo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+import logo from "../../assets/logo.png";
+
+const Logo = () => <img className="logo" src={logo} alt="Logo da CollabCode" />;
+
+export default Logo;
