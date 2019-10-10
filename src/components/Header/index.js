e78e6af73f7028c5b7939aedf595d3b6b13a3590
mmn92 <matheus.mota.nogueira@gmail.com>
2019-10-10 12:15:43 -0300

    create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "../../objects/Logo";
+import Sobre from "../../objects/Sobre";
+import Menu from "../../objects/Menu";
+
+import "./styles.css";
+
+const Header = () => (
+  <header className="header">
+    <Logo />
+    <Sobre />
+    <Menu />
+  </header>
+);
+
+export default Header;
