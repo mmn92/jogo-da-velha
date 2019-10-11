e5a6ac457ca38b2f2477c00b0e1fd7c4e8c30c80
mmn92 <matheus.mota.nogueira@gmail.com>
2019-10-11 18:15:11 -0300

    cria simbolos X e O

---
+++ src/objects/SymbolO/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./styles.css";
+import Circle from "../../assets/Circle.png";
+
+const SymbolO = () => <img className="symbol-o" src={Circle} alt="Símbolo O" />;
+
+export default SymbolO;
---
+++ src/objects/SymbolX/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./styles.css";
+import X from "../../assets/X.png";
+
+const SymbolX = () => <img className="symbol-x" src={X} alt="Símbolo X" />;
+
+export default SymbolX;
