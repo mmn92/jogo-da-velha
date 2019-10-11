cc6129332d282e335f6c4803055d6886aa990094
mmn92 <matheus.mota.nogueira@gmail.com>
2019-10-11 16:13:00 -0300

    renderiza Header no App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Sobre from "./objects/Sobre";
+
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Sobre />
+      <Header />
     </>
   );
 }
